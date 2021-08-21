22e5883a2325246209ab355f14c391e665e5febb
Avisa-SEI <aap571420@gmail.com>
2021-08-21 10:45:15 -0700

    wip: add iconButtons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,39 @@
-import { AppBar } from '@material-ui/core'
-import React from 'react'
+import React from "react";
+import {
+  AppBar,
+  Badge,
+  Grid,
+  IconButton,
+  InputBase,
+  Toolbar,
+} from "@material-ui/core";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 
 export default function Header() {
-    return (
-        <div>
-            <AppBar>
-                
-            </AppBar>
-        </div>
-    )
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container>
+            <Grid item sm={6} style={{ border: "1px solid #fff" }}>
+              <InputBase />
+            </Grid>
+            <Grid item sm={6} style={{ border: "1px solid #000" }}>
+              <IconButton>
+                <Badge badgeContent={4}>
+                  <NotificationsNoneIcon />
+                </Badge>
+              </IconButton>
+              <IconButton>
+                <Badge badgeContent={2}>
+                  <ChatBubbleOutlineIcon />
+                </Badge>
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
