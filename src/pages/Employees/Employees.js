dffb41f84c11533839424fc73e81183b12f92f19
Avisa-SEI <aap571420@gmail.com>
2021-09-01 09:00:12 -0700

    feat: add a table and render data into tabel

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import EmployeeForm from "./EmployeeForm";
 import PeopleOutlineIcon from "@material-ui/icons/PeopleOutline";
 import PageHeader from "../../components/PageHeader";
-import { makeStyles, Paper } from "@material-ui/core";
+import {
+  makeStyles,
+  Paper,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@material-ui/core";
+import useTable from "../../components/useTable";
+import * as employeeService from "../../services/employeeService";
 
 const useStyles = makeStyles((theme) => ({
   pageContent: {
@@ -13,6 +21,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Employees() {
   const classes = useStyles();
+  const [records, setRecords] = useState(employeeService.getAllEmployees);
+  const { TblContainer } = useTable();
   return (
     <>
       <PageHeader
@@ -22,6 +32,20 @@ export default function Employees() {
       />
       <Paper className={classes.pageContent}>
         <EmployeeForm />
+      </Paper>
+      <Paper className={classes.pageContent}>
+        <TblContainer>
+          <TableBody>
+            {records.map((record) => (
+              <TableRow key={record.id}>
+                <TableCell>{record.fullName}</TableCell>
+                <TableCell>{record.email}</TableCell>
+                <TableCell>{record.mobile}</TableCell>
+                <TableCell>{record.department}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TblContainer>
       </Paper>
     </>
   );
--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -29,5 +29,10 @@ export function generateEmployeeId() {
 export function getAllEmployees() {
   if (localStorage.getItem(KEYS.employees) == null)
     localStorage.setItem(KEYS.employees, JSON.stringify([]));
-  return JSON.parse(localStorage.getItem(KEYS.employees));
+  let employees = JSON.parse(localStorage.getItem(KEYS.employees));
+  let deparments = getDepartmentCollection();
+  return employees.map((employee) => ({
+    ...employee,
+    department: deparments[employee.departmentId - 1].title,
+  }));
 }
